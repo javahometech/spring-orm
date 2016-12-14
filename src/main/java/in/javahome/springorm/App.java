ad8ca2bc9db2d996c11ac31a4c16c924e4509dc1
Hari <hari@javahome.in>
2016-12-14 09:55:04 +0530

    Transactions examples

--- src/main/java/in/javahome/springorm/App.java
+++ src/main/java/in/javahome/springorm/App.java
@@ -1,24 +1,21 @@
 package in.javahome.springorm;
 
-import java.sql.SQLException;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import in.javahome.springorm.dao.EmployeeDAO;
-import in.javahome.springorm.entity.Employee;
 
 /**
  * Hello world!
  *
  */
 public class App {
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws Exception {
 		ApplicationContext cxt = new ClassPathXmlApplicationContext("spring-orm.xml");
 		EmployeeDAO employeeDAO = cxt.getBean(EmployeeDAO.class);
 //		Employee emp = new Employee();
-//		emp.setEmpName("Raghu");
+//		emp.setEmpName("Hari");
 //		emp.setPhone("999999999");
-		employeeDAO.deleteEmployee(3);
+		employeeDAO.deleteEmployee(5);
 	}
 }
